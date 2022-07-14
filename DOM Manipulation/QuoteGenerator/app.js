9c15ae2445d628004444be81f79c081d9144408f
Tarun Lunia <tarunlunia1998@gmail.com>
2022-07-14 22:43:39 +0530

    --added Quote Generator App

---
+++ DOM Manipulation/QuoteGenerator/app.js
@@ -0,0 +1,49 @@
+
+
+let quote = document.querySelector('.quote')
+
+let person = document.querySelector('.person')
+
+let btn = document.querySelector('#new-quote')
+
+
+const quotesArray = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    person: 'Nelson Mandela'
+  },
+  {
+    quote: 'Don\'t judge each day by the harvest you reap but by the seeds that you plant.',
+    person: 'Robert Louis Stevenson'
+  },
+  {
+    quote: 'Whoever is happy will make others happy too',
+    person: 'Anne Frank'
+  },
+  {
+    quote: 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
+    person: 'Oprah Winfrey'
+  },
+  {
+    quote: 'Life is what happens when you\'re busy making other plans.',
+    person: 'John Lennon'
+  },
+  {
+    quote: 'If life were predictable it would cease to be life, and be without flavor.',
+    person: 'Eleanor Roosevelt'
+  },
+  {
+    quote: 'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma which is living with the results of other people\'s thinking.',
+    person: 'Steve Jobs'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing',
+    person: 'Walt Disney'
+  }
+]
+
+btn.addEventListener('click', () => {
+  let random = Math.floor(Math.random() * (quotesArray.length))
+  quote.innerHTML = quotesArray[random].quote
+  person.innerHTML = quotesArray[random].person
+})
